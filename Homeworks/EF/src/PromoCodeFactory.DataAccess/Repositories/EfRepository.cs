1a3adb995f7f823263e222b1bcc3ca10461afe91
urmatgit <urmat2g@gmail.com>
2025-03-19 11:27:45 +0300

    Added packages(Ef, ef Sqlite) and  db context, repository

---
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Repositories/EfRepository.cs
@@ -0,0 +1,23 @@
+﻿using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoCodeFactory.DataAccess.Repositories
+{
+    public class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
