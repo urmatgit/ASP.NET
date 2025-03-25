cdf8fe59627afc0793809f26c597619fd7591855
urmatgit <urmat2g@gmail.com>
2025-03-25 16:54:14 +0300

    add test (пункты 1,2,3)

---
+++ Homeworks/UnitTests/src/TestProjectHomeWork_xUnit/TestFixture.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PromoCodeFactory.WebHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectHomeWork_xUnit
+{
+    public class TestFixture:IDisposable
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+        public TestFixture()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var configuration = configurationBuilder.Build();
+            var servicesCollection = new ServiceCollection();
+            new Startup(configuration).ConfigureServices(servicesCollection);
+            var serviceProvider = servicesCollection.BuildServiceProvider();
+            ServiceProvider = serviceProvider;
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
